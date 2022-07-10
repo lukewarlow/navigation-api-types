47724b39cea7543034fd533909ee5594c799c3df
Luke Warlow <projects@warlow.dev>
2022-07-10 13:45:01 +0100

    Deprecate transitionWhile/canTransition and add intercept/canIntercept
    Improved jsdoc comments

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 // WICG Spec: https://github.com/WICG/navigation-api
 
-// https://wicg.github.io/navigation-api/#navigation
+/** @see https://wicg.github.io/navigation-api/#navigation */
 interface Navigation extends EventTarget {
     entries(): NavigationHistoryEntry[];
     readonly currentEntry?: NavigationHistoryEntry;
@@ -33,11 +33,13 @@ declare interface WindowNavigation {
     readonly navigation?: Navigation;
 }
 
-// https://wicg.github.io/navigation-api/#navigateevent
+/** @see https://wicg.github.io/navigation-api/#navigateevent */
 interface NavigateEvent extends Event {
     readonly navigationType: NavigationApiNavigationType;
     readonly destination: NavigationDestination;
+    /** @deprecated Deprecated in favour of {@link canIntercept} */
     readonly canTransition: boolean;
+    readonly canIntercept: boolean;
     readonly userInitiated: boolean;
     readonly hashChange: boolean;
     readonly signal: AbortSignal;
@@ -45,7 +47,9 @@ interface NavigateEvent extends Event {
     readonly downloadRequest?: string;
     readonly info: any;
 
+    /** @deprecated Deprecated in favour of {@link intercept} */
     transitionWhile(newNavigationAction: Promise<void>, options?: NavigationTransitionWhileOptions): void;
+    intercept(options?: NavigationInterceptOptions): void;
     restoreScroll(): void;
 }
 
@@ -54,11 +58,13 @@ declare var NavigateEvent: {
     new(type: string, eventInit: NavigateEventInit): Event;
 };
 
-// https://wicg.github.io/navigation-api/#dictdef-navigateeventinit
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigateeventinit */
 interface NavigateEventInit extends EventInit {
     navigationType?: NavigationType;
     destination: NavigationDestination;
+    /** @deprecated Deprecated in favour of {@link canIntercept} */
     canTransition?: boolean;
+    canIntercept?: boolean;
     userInitiated?: boolean;
     hashChange?: boolean;
     signal: AbortSignal;
@@ -67,7 +73,7 @@ interface NavigateEventInit extends EventInit {
     info?: any;
 }
 
-// https://wicg.github.io/navigation-api/#navigationcurrententrychangeevent
+/** @see https://wicg.github.io/navigation-api/#navigationcurrententrychangeevent */
 interface NavigationCurrentEntryChangeEvent extends Event {
     readonly navigationType?: NavigationApiNavigationType;
     readonly from: NavigationHistoryEntry;
@@ -78,13 +84,13 @@ declare var NavigationCurrentEntryChangeEvent: {
     new(type: string, eventInit: NavigationCurrentEntryChangeEventInit): Event;
 };
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationcurrententrychangeeventinit
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationcurrententrychangeeventinit */
 interface NavigationCurrentEntryChangeEventInit extends EventInit {
     navigationType?: NavigationType;
     destination: NavigationHistoryEntry;
 }
 
-// https://wicg.github.io/navigation-api/#navigationhistoryentry
+/** @see https://wicg.github.io/navigation-api/#navigationhistoryentry */
 interface NavigationHistoryEntry extends EventTarget {
     readonly url?: string;
     readonly key: string;
@@ -97,7 +103,7 @@ interface NavigationHistoryEntry extends EventTarget {
     ondispose: ((this: NavigationHistoryEntry, ev: Event) => any) | null;
 }
 
-// https://wicg.github.io/navigation-api/#navigationdestination
+/** @see https://wicg.github.io/navigation-api/#navigationdestination */
 interface NavigationDestination {
     readonly url: string;
     readonly key?: string;
@@ -108,55 +114,65 @@ interface NavigationDestination {
     getState(): any;
 }
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationupdatecurrententryoptions
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationupdatecurrententryoptions */
 interface NavigationUpdateCurrentEntryOptions {
     state: any;
 }
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationoptions
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationoptions */
 interface NavigationOptions {
     info?: any;
 }
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationnavigateoptions
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationnavigateoptions */
 interface NavigationNavigateOptions extends NavigationOptions {
     state?: any;
     // Defaults to "auto"
     history?: NavigationHistoryBehavior;
 }
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationreloadoptions
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationreloadoptions */
 interface NavigationReloadOptions extends NavigationOptions {
     state?: any;
 }
 
-// https://wicg.github.io/navigation-api/#navigationtransition
+/** @see https://wicg.github.io/navigation-api/#navigationtransition */
 interface NavigationTransition {
     readonly navigationType: NavigationApiNavigationType;
     readonly from: NavigationHistoryEntry;
     readonly finished: Promise<void>;
 }
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationresult
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationresult */
 interface NavigationResult {
     committed: Promise<NavigationHistoryEntry>;
     finished: Promise<NavigationHistoryEntry>;
 }
 
-// https://wicg.github.io/navigation-api/#dictdef-navigationtransitionwhileoptions
+/** @deprecated Deprecated in favour of {@link NavigationInterceptOptions} */
 interface NavigationTransitionWhileOptions {
     focusReset?: NavigationFocusReset;
     scrollRestoration?: NavigationScrollRestoration;
 }
 
-// https://wicg.github.io/navigation-api/#enumdef-navigationtype
+/** @see https://wicg.github.io/navigation-api/#dictdef-navigationinterceptoptions */
+interface NavigationInterceptOptions {
+    handler?: NavigationInterceptHandler;
+    focusReset?: NavigationFocusReset;
+    scrollRestoration?: NavigationScrollRestoration;
+}
+
+/** @see https://wicg.github.io/navigation-api/#enumdef-navigationtype */
 type NavigationApiNavigationType = "reload" | "push" | "replace" | "traverse";
 
-// https://wicg.github.io/navigation-api/#enumdef-navigationhistorybehavior
+/** @see https://wicg.github.io/navigation-api/#enumdef-navigationhistorybehavior */
 type NavigationHistoryBehavior = "auto" | "push" | "replace";
 
-// https://wicg.github.io/navigation-api/#enumdef-navigationfocusreset
+/** @see https://wicg.github.io/navigation-api/#enumdef-navigationintercepthandler */
+type NavigationInterceptHandler = Promise<void>;
+
+/** @see https://wicg.github.io/navigation-api/#enumdef-navigationfocusreset */
 type NavigationFocusReset = "after-transition" | "manual";
 
-// https://wicg.github.io/navigation-api/#enumdef-navigationscrollrestoration
+/** @see https://wicg.github.io/navigation-api/#enumdef-navigationscrollrestoration */
 type NavigationScrollRestoration = "after-transition" | "manual";
